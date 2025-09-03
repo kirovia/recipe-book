72eb793510e7842d8db93a6374c65c7ebd5ed6c1
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-09-03 11:00:09 -0400

    Finishes basic setup, file structuring, and boilerplate code

---
+++ lib/models/category.py
@@ -0,0 +1,7 @@
+# lib/models/category.py
+from models.__init__ import CURSOR, CONN
+
+class Category:
+
+    def __init__(self):
+        pass
---
+++ lib/models/recipe.py
@@ -0,0 +1,7 @@
+# lib/models/recipe.py
+from models.__init__ import CURSOR, CONN
+
+class Recipe:
+
+    def __init__(self):
+        pass
