3c7795992db49d2162510ac2aea4c2a022624de9
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-09-03 20:18:16 -0400

    Adds boilerplate CLI code and functions

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,16 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    view_all_categories,
+    get_category,
+    create_category,
+    delete_category,
+    get_category_recipes,
+    view_all_recipes,
+    get_recipe,
+    create_recipe,
+    delete_recipe,
+    view_recipe_category
 )
 
 
@@ -13,15 +22,44 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            view_all_categories()
+        elif choice == "2":
+            get_category()
+        elif choice == "3":
+            create_category()
+        elif choice == "4":
+            delete_category()
+        elif choice == "5":
+            get_category_recipes()
+        elif choice == "6":
+            view_all_recipes()
+        elif choice == "7":
+            get_recipe()
+        elif choice == "8":
+            create_recipe()
+        elif choice == "9":
+            delete_recipe()
+        elif choice == "10":
+            view_recipe_category()
         else:
             print("Invalid choice")
 
 
 def menu():
     print("Please select an option:")
+    print("*~*~*~*~*~*~*~*~*~*")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. View all categories")
+    print("2. Find category by id or name")
+    print("3. Create new category")
+    print("4. Delete category")
+    print("5. View all recipes from single category")
+    print("6. View all recipes")
+    print("7. Find recipe by id or name")
+    print("8. Add new recipe")
+    print("9. Delete recipe")
+    print("10. View a recipe's category")
+    print("*~*~*~*~*~*~*~*~*~*")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,8 +1,34 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+def view_all_categories():
+    print('\nviewing all categories\n')
 
+def get_category():
+    print('\ngetting category\n')
+
+def create_category():
+    print('\ncreating category\n')
+
+def delete_category():
+    print('\ndeleting category\n')
+
+def get_category_recipes():
+    print('\ngetting all recipes from category\n')
+
+def view_all_recipes():
+    print('\nviewing all recipes\n')
+
+def get_recipe():
+    print('\ngetting recipe\n')
+
+def create_recipe():
+    print('\ncreating recipe\n')
+
+def delete_recipe():
+    print('\ndeleting recipe\n')
+
+def view_recipe_category():
+    print('\nviewing recipe category\n')
 
 def exit_program():
     print("Goodbye!")
